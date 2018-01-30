5be17a90bb650d180028da8d724e3c5c2580b806
Logan Karney <karneyl@eos10.cis.gvsu.edu>
2018-01-30 17:42:51 -0500

    Started instantiation of function.h file.

--- project1/functions.c
+++ project1/functions.c
@@ -1,7 +1,12 @@
 #include "functions.h"
+#include <stdlib.h>
 
 int main(int argc, char* argv[]){
 	int choice = -1;
+	FILE *fin, *fout;
+	
+	//put down below with other error
+	//if(argc != 5)
 
 	char *myString = argv[1];
 	
@@ -10,11 +15,23 @@ int main(int argc, char* argv[]){
 	else if(myString[0] == 'd')
 		choice = 1;
 	
-	printf(argv[1]);
-	printf("\n");
-	printf("%d", choice);
-	printf("\n");
-
+	if(choice == -1){
+		printf("Error: Please enter a valid input.\n");
+		exit(1);
+	}
 
 	return 0;
+}
+
+char * removeDuplicates(char word[]){
+	char* used = "";
+	int len = strlen(word);
+	int i = 0;
+	while(i < len){
+		if(strchr(used, word){
+		}
+		i++;
+	}
+
+	return '0';
 }
