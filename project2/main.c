6823c5de84674b91100d416f7fd03e20869f436e
Logan Karney <karneyl@eos14.cis.gvsu.edu>
2018-02-25 16:17:41 -0500

    Added external file functionality

--- project2/main.c
+++ project2/main.c
@@ -2,8 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include "operations.h"
-
-void print_messages();
+#include "operations.c"
 
 int main(int argc, char* argv[]){
 
@@ -18,36 +17,8 @@ int main(int argc, char* argv[]){
 //	printf("You have chosen %d\n", choice);
 	printf("\n");
 
-	ITEM item;
+	product product;
 	
 }
 
-void print_messages(){
-	char border[] = "==========================================================";
 
-
-        char* options[] = {
-                                "Add product to store" , "Purchase product from store",
-                                "Check price of a product", "Show products in store",
-                                "Clean up a product from store", "Find product",
-                                "Inventory", "Done for today"
-
-                        };
-
-        char welcome_message[] = "please enter a number ";
-
-        int options_size = ((char) sizeof(options)/sizeof(options[0]));
-
-
-        printf("%s[%d-%d]\n%s\n", welcome_message, 1, options_size, border);
-
-        int i = 0;
-
-        //Allows for increase in menu options
-        //FOR MODULIZATION JUST PRINT IT ONE BY ONE, I++, BUT WHENEVER I % 2 == 0, PRINT A NEW LINE
-        for(i = 0; i < options_size; i+=2){
-                printf("%d%s%-32s%d%s%-32s\n", i+1, ": ", options[i], i+2, ": ", options[i+1]);
-        }
-
-        printf("%s\n", border);
-}
