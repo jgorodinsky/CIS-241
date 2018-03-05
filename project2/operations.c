36129e99ee7788042a1f782ea3d93835b35e8cf2
Logan Karney <karneyl@eos14.cis.gvsu.edu>
2018-03-05 18:00:01 -0500

    Added method code.

--- project2/operations.c
+++ project2/operations.c
@@ -1,7 +1,11 @@
 #include "operations.h"
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void print_messages(){
+	system("clear");
+
         char border[] = "==========================================================";
 
 
@@ -13,7 +17,7 @@ void print_messages(){
 
                         };
 
-        char welcome_message[] = "please enter a number ";
+        char welcome_message[] = "Welcome to Kroger++\nplease enter a number ";
 
         int options_size = ((char) sizeof(options)/sizeof(options[0]));
 
@@ -31,3 +35,57 @@ void print_messages(){
         printf("%s\n", border);
 }
 
+int insert(product **l, product node){
+	return 0;
+}
+
+void rmItem(product *l, product *node){	
+}
+
+void showList(product *l){
+	
+}
+
+int loadData(char inf[], product **l){
+	return  0;
+}
+
+int saveData(char outf[], product *l){
+	//save data to file
+	
+	//free data
+	struct product* tmp;
+        while(l != NULL){
+                tmp = l;
+                l = l -> next;
+                free(tmp);
+        }
+
+	return 0;
+}
+
+float purchase(product *l, char product[], float q){
+	return q;
+}
+
+void check_price(product *l, char p[]){
+}
+
+void findItem(product *l, char p[]){
+}
+
+int doIt(char data[]){
+	int choice = -1;
+
+	//load data
+	
+	//ask input
+	while(choice != 0){
+		print_messages();	
+		scanf("%d", &choice);
+		//switch statement
+	}
+	//save data
+
+	return 0;
+}
