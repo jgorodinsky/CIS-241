e35b9f43368a94bfd826b09e574109d43175b960
Logan Karney <karneyl@eos14.cis.gvsu.edu>
2018-03-30 00:50:35 -0400

    Improved flag parsing.

--- project3/backup.sh
+++ project3/backup.sh
@@ -1,32 +1,48 @@
+l_f=false
+n_f=false 
+help_f=false
+
+
 echo
 
 #Iterates through each argument passed to the script
 for var in "$@"
 do
 
-#Checks if argument is a script option
+#Checks if argument is a script flag
 if [[ $var = "-"* ]]; then
 	
 	#Checks if $var equals --help flag
 	if [[ $var = "--help" ]]; then
+		help_f=true
 		echo help statement requested
 
 
 	else
-		#Checks if $var matches other flags
-		if [[ $var = *"l"* ]]; then
-
-			echo list of backup dir contents requested
-
-		fi
-		
-		if [[ $var = *"n"* ]]; then
+		#Checks if each character of var matches other flags
+		for ((i=1; i <${#var}; i++)); do
 			
-			echo number of files and mem used requested
+			opt=${var:$i:1}
+			
 
-		fi
+			if [[ $opt = "l" ]]; then
+
+				echo list of backup dir contents requested
+				l_f=true
+			
+		
+			elif [[ $opt = "n" ]]; then
+			
+				echo number of files and mem used requested
+				n_f=true
+
+			else
+				echo backup.sh: invalid option $opt
+			fi
+
+
+		done
 	fi
-
 fi
 
 #Exits for loop
