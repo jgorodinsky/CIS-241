ff91e2758e5ea0d006c235479d84c98af88459ac
Logan Karney <karneyl@eos14.cis.gvsu.edu>
2018-04-08 22:38:48 -0400

    Finished flag options.

--- project3/backup.sh
+++ project3/backup.sh
@@ -3,7 +3,7 @@
 l_f=false
 n_f=false 
 help_f=false
-
+echo_switch=false
 
 #Creates .backup directory if it doesn't exist
 mkdir -p "~/.backup"
@@ -18,6 +18,7 @@ if [[ $var = "-"* ]]; then
 	#Checks if $var equals --help flag
 	if [[ $var = "--help" ]]; then
 		help_f=true
+		echo_switch=true
 
 	else
 		#Checks if each character of var matches other flags
@@ -27,13 +28,12 @@ if [[ $var = "-"* ]]; then
 			
 
 			if [[ $opt = "l" ]]; then
-
 				l_f=true
-			
+				echo_switch=true
 		
 			elif [[ $opt = "n" ]]; then
-			
 				n_f=true
+				echo_switch=true;
 
 			else
 				echo backup.sh: invalid option $opt
@@ -51,17 +51,26 @@ else
 		cp -r $var ~/.backup
 	fi
 fi
+
+#Exits for loop
+done
+
+if [[ $echo_switch = true ]]; then
+	echo
+fi
+
 if [[ $l_f = true ]]; then
-	echo list of backup dir contents requested
+	ls -R ~/.backup
 fi
 
 if [[ $n_f = true ]]; then
-	echo number of files and mem used requested
-
+	ls -Rl ~/.backup
 fi
 
 if [ $help_f = true ]; then
 	echo help statement requested
 fi
-#Exits for loop
-done
+
+if [[ $echo_switch = true ]]; then
+        echo
+fi
